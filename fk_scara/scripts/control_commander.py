9a395d2052144e15d8ed3e7ac47c8459d716111d
bhavikmk <u19me151@med.svnit.ac.in>
2022-08-11 17:57:16 +0530

    added code : joint position controller

--- fk_scara/scripts/control_commander.py
+++ fk_scara/scripts/control_commander.py
@@ -4,45 +4,36 @@ from math import pi
 import rospy
 from std_msgs.msg import Float64
 import matplotlib.pyplot as plt
-#from trajectory_msgs.msg import JointTrajectory
-# theta1, theta2 and d are joint angles of robot
+from trajectory_msgs.msg import JointTrajectory
+from sensor_msgs.msg import JointState
 
+pose = rospy.get_param('poses/goal_pose')
 
-def control_commander():
-    pub1 = rospy.Publisher('/scara_control/joint1_position_controller/command', Float64, queue_size=10)
-    pub2 = rospy.Publisher('/scara_control/joint2_position_controller/command', Float64, queue_size=10)
-    pub3 = rospy.Publisher('/scara_control/joint3_position_controller/command', Float64, queue_size=10)    
+prev_state = JointState()
+curr_state = JointState()
+
+def callback(data):
+
+    global pose
+
+    curr_state = data
+    e = np.array([prev_state[0] - curr_state.position[0], prev_state[1] - curr_state.position[1], prev_state[2] - curr_state.position[2]])
+
+    cmd_pos1 = pose[0] - data.position[0]
+    cmd_pos2 = pose[1] - data.position[1]
+    cmd_pos3 = pose[2] - data.position[2]
     
-    rospy.init_node('control_commander', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        #traj1 = JointTrajectory()
-        cmd_pos1 = -1.0
-        cmd_pos2 = - 1.0
-        cmd_pos3 = 1.0
-        x = [0,1,2,3,4]
-        fig, axs = plt.subplots(2, 2)
-        '''
-        axs[0, 0].plot(cmd_pos1)
-        axs[0, 0].set_title('Axis [0, 0]')
-        axs[0, 1].plot(i, y, 'tab:orange')
-        axs[0, 1].set_title('Axis [0, 1]')
-        axs[1, 0].plot(x, -y, 'tab:green')
-        axs[1, 0].set_title('Axis [1, 0]')
-        axs[1, 1].plot(x, -y, 'tab:red')
-        axs[1, 1].set_title('Axis [1, 1]')
-        '''
+    prev_state = data
 
-           
-        # rospy.loginfo(cmd_pos1)
-        pub1.publish(cmd_pos1)
-        pub2.publish(cmd_pos2)
-        pub3.publish(cmd_pos3)
-        
-        rate.sleep()
+    pub1.publish(cmd_pos1)
+    pub2.publish(cmd_pos2)
+    pub3.publish(cmd_pos3)
 
-if __name__ == '__main__':
-    try:
-        control_commander()
-    except rospy.ROSInterruptException:
-        pass
+
+rospy.init_node('scara_planner', anonymous=True)
+
+pub1 = rospy.Publisher('/scara_control/joint1_position_controller/command', Float64, queue_size=10)
+pub2 = rospy.Publisher('/scara_control/joint2_position_controller/command', Float64, queue_size=10)
+pub3 = rospy.Publisher('/scara_control/joint3_position_controller/command', Float64, queue_size=10)    
+sub = rospy.Subscriber('/joint_states', JointState, callback)
+rospy.spin()
